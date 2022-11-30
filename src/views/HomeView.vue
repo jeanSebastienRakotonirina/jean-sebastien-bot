a91ae75837672de5443d944a238f0bcac7d866db
jeanSebastienRakotonirina <jsr029.applyforjob@gmail.com>
2022-11-30 15:26:56 +0100

    i

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,39 +1,47 @@
 <template>
-  <div class="home">
-    <div class="card" style="width: 18rem;">
-      <img src="" class="card-img-top" alt="">
-      <div class="card-body">
-        <h5 class="card-title">{{ user.title }}</h5>
-        <img :src="user.imgSrc" class="card-text" />
-        <a v-bind:href="user.url" class="btn btn-primary">Go somewhere</a>
+  <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5"
+  style="width:100%; margin: 0 auto;"
+  >
+    <div class="col" v-for="pro in projects" :key="pro.id">
+      <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg"
+        :style="{ background: 'url(' + pro.imgSrc + ') no-repeat'}"
+        style="background-size: cover; border-radius: 10px !important;padding: 2px;border: 2px solid black;"
+        >
+        <div class="d-flex flex-column h-100 p-5 pb-3 text-blue text-shadow-1">
+          <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold"
+          style="text-shadow: 2px 2px 2px green;"
+          >{{ pro.title }}</h3>
+          <ul class="-flex list-unstyled mt-auto">
+            <button @click="handleClick(pro.url)" class="btn btn-success">+ Détails</button>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
 </template>
-
 <script>
-import axios from 'axios'
-
 export default {
   name: 'HomeView',
   components: {
   },
-  data () {
+  data: function () {
     return {
-      user: {}
+      projects: {}
     }
   },
-  async mounted () {
-    const res = await axios({
-      method: 'get',
-      url: 'https://github.com/jsr029/rjs-portfolio/blob/master/portfolio.json',
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '/'
-      },
-      crossdomain: true
-    })
-    this.user = await res.json()
+  computed: {
+    styles: function () {
+      return this.projects.map(m => m.imgSrc)
+    }
+  },
+  mounted: async function () {
+    const res = await fetch('https://jeansebastienrakotonirina.github.io/portfolioApi/projects.json')
+    this.projects = await res.json()
+  },
+  methods: {
+    handleClick: function (url) {
+      window.open(url)
+    }
   }
 }
 </script>
